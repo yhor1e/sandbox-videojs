2c6c5ea1e4c959f006b308c2b8cc7c06fc0813df
yhorie <yuk.horie@gmail.com>
2018-10-05 13:33:02 +0900

    Polish

--- src/main.js
+++ src/main.js
@@ -1,52 +1,87 @@
 (function(){
 
-  var dataList = window.dataList = [
-    {
-      time: 1,
-      description: 'foo'
-    },
-    {
-      time: 3,
-      description: 'bar'
-    }
-  ];
+  var options = {
+    controls: true,
+    inactivityTimeout: 0
+  };
 
   window.addEventListener('DOMContentLoaded', function(){
-    console.log('DOMcontentLoaded');
 
-    // create description list from dataList
-    var $description = $('#description');
-    var $ul = $('<ul>');
-    dataList.forEach(function(data){
-      var $li = $('<li>');
-      var $a = $('<a>');
-      $a.attr({
-        'id': data.time,
-        'href': '#' + data.time + 'sec'
-      });
-      $a.text(data.description);
-      $li.append($a);
-      $ul.append($li);
+    var descriptions1 = [
+      {
+        time: '00:01',
+        description: 'foo'
+      },
+      {
+        time: '00:03',
+        description: 'bar'
+      },
+      {
+        time: '00:04',
+        description: 'foo'
+      },
+      {
+        time: '00:06',
+        description: 'bar'
+      },
+      {
+        time: '00:08',
+        description: 'bar'
+      },
+      {
+        time: '00:09',
+        description: 'bar'
+      },
+      {
+        time: '00:10',
+        description: 'bar'
+      }
+
+    ];
+
+    var v1 = videojs('video1', options);
+    createDescriptionEl( v1, 'description1', descriptions1);
+    v1.on('timeupdate', function(){
+      scrollAndHighlightDesc(v1, 'description1', descriptions1)
     });
-    $description.append($ul);
-    $ul.on('click', 'a', function(e){
-      v.currentTime(parseInt(e.target.id, 10));
-      v.play();
-    })
 
-    // initialize videojs instance
-    window.v = videojs('my-video');
-    v.on('timeupdate', function(e){
+
+    // ----------------------------------------------------
+    // Create description area
+    // ----------------------------------------------------
+    function createDescriptionEl(v, descAreaId, dataList) {
+      var $description = $('#' + descAreaId);
+      var $ul = $('<ul>');
+      dataList.forEach(function(data){
+        var $li = $('<li>');
+        $li.attr('data-sec', moment(data.time, 'mm:ss').second());
+        $li.attr('id', descAreaId + '-' + moment(data.time, 'mm:ss').second());
+        $li.text(data.time + '  ' +data.description);
+        $ul.append($li);
+      });
+      $description.append($ul);
+      $ul.on('click', 'li', function(e){
+        v.currentTime(parseInt($(e.target).attr('data-sec'), 10));
+        v.play();
+      })
+    };
+
+    // ----------------------------------------------------
+    // Description scroll and highlight
+    // ----------------------------------------------------
+    function scrollAndHighlightDesc(v, descAreaId, dataList){
       var currentTimeInt = parseInt(v.currentTime(), 10);
       var data = dataList.filter(function(d){
-        return d.time === currentTimeInt;
+        return moment(d.time, 'mm:ss').second() === currentTimeInt;
       });
       if(data[0]){
-        console.log(data[0].time, data[0].description);
-        var $active = $('#' + data[0].time);
-        $('#description a').removeClass('active')
+        var $active = $('#' + descAreaId + '-' + moment(data[0].time, 'mm:ss').second());
+        $('#' + descAreaId + ' li').removeClass('active')
         $active.addClass('active');
+        var offsetY = 10;
+        var scrollPosY = $active[0].offsetTop - $('#' + descAreaId)[0].offsetTop - offsetY;
+        $('#' + descAreaId).scrollTop(scrollPosY);
       }
-    });
+    }
   })
 })();
